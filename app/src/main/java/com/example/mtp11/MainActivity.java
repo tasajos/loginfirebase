4bffd73eb6a7258c588582371a88894a9fc8352b
tasajos <tasajos@gmail.com>
2023-04-11 12:53:26 -0400

    Version 1.5

--- app/src/main/java/com/example/mtp11/MainActivity.java
+++ app/src/main/java/com/example/mtp11/MainActivity.java
@@ -7,18 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_add;
-    Button btn_listar;
+    Button btn_listar,btn_cerrar;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btn_add = findViewById(R.id.btn_add);
+        mAuth = FirebaseAuth.getInstance();
         btn_listar = findViewById(R.id.btn_listar);
-
+        btn_cerrar = findViewById(R.id.btn_cerrar);
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,5 +37,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            mAuth.signOut();
+            finish();
+            startActivity(new Intent(MainActivity.this,login.class));
+            }
+        });
     }
 }
