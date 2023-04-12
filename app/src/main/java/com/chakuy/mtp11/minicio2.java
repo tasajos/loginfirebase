101d7525d958a7911fb18418f48b3d0b6c33c17b
tasajos <tasajos@gmail.com>
2023-04-12 16:13:28 -0400

    Version 1.7 update version android

---
+++ app/src/main/java/com/chakuy/mtp11/minicio2.java
@@ -0,0 +1,14 @@
+package com.chakuy.mtp11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class minicio2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minicio2);
+    }
+}
