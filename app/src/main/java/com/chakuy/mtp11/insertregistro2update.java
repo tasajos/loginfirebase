13601f3dce88b8179b2e852cd9278d7485450346
tasajos <tasajos@gmail.com>
2023-07-09 17:23:22 -0400

    Version 6.1 formulario UI

---
+++ app/src/main/java/com/chakuy/mtp11/insertregistro2update.java
@@ -0,0 +1,14 @@
+package com.chakuy.mtp11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class insertregistro2update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertregistro2update);
+    }
+}
