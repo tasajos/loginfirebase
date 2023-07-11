d6fb3f9aa99541ced1f89aed18db189128b2528f
tasajos <tasajos@gmail.com>
2023-07-11 10:57:43 -0400

    Version 6 - Jucumari

--- app/src/main/java/com/chakuy/mtp11/insertregistro2update.java
+++ app/src/main/java/com/chakuy/mtp11/insertregistro2update.java
@@ -1,22 +1,20 @@
 package com.chakuy.mtp11;
 
-import android.os.Bundle;
-import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -26,99 +24,128 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.Arrays;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
+public class insertregistro2update extends AppCompatActivity implements OnMapReadyCallback {
 
-public class insertregistro2update extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
-    Button btnalarma;
-    EditText ubicacion;
+    private EditText nombre, apellido, detalle, pedido, cantidad, color, precio, ubicacion, fecha;
+    private Button btnalarma;
+
+    private ImageButton btn_registrar;
+    private Spinner tipoSpinner;
+
+    private FirebaseFirestore mfirestore;
     private FusedLocationProviderClient fusedLocationClient;
     private Location lastKnownLocation;
-    private static final int PERMISSION_REQUEST_CODE = 200;
     private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insertregistro2update);
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        this.setTitle("Registro Ubicacion");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mfirestore = FirebaseFirestore.getInstance();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.setTitle("Registro Ubicacion");
+
+        nombre = findViewById(R.id.nombre);
+        apellido = findViewById(R.id.apellido);
+        tipoSpinner = findViewById(R.id.tipo);
+        detalle = findViewById(R.id.detalle);
+        pedido = findViewById(R.id.pedido);
+        cantidad = findViewById(R.id.cantidad);
+        color = findViewById(R.id.color);
+        precio = findViewById(R.id.precio);
         ubicacion = findViewById(R.id.ubicacion);
+        fecha = findViewById(R.id.fecha);
+        btn_registrar = findViewById(R.id.btn_registrar);
         btnalarma = findViewById(R.id.btnalarma);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tipos_producto, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        tipoSpinner.setAdapter(adapter);
 
         btnalarma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String message = ubicacion.getText().toString();
-                //sendWhatsAppMessages(Arrays.asList("+59177087685","+59163962491"), message); // Handle button click if needed
+                // Envía mensajes de WhatsApp si es necesario
+                //sendWhatsAppMessages(Arrays.asList("+59177087685", "+59163962491"), message);
             }
         });
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView2);
-        // Create a new instance of OnMapReadyCallback and implement the onMapReady() method
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap map) {
-                googleMap = map;
 
-                // Call a method to update the map with the location
-                updateMapLocation();
+        btn_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nombre.getText().toString().trim();
+                String lastName = apellido.getText().toString().trim();
+                String type = tipoSpinner.getSelectedItem().toString().trim();
+                String detail = detalle.getText().toString().trim();
+                String order = pedido.getText().toString().trim();
+                String quantity = cantidad.getText().toString().trim();
+                String colorValue = color.getText().toString().trim();
+                String price = precio.getText().toString().trim();
+                String locationValue = ubicacion.getText().toString().trim();
+                String date = fecha.getText().toString().trim();
+
+                if (name.isEmpty() || lastName.isEmpty() || type.isEmpty() || detail.isEmpty() ||
+                        order.isEmpty() || quantity.isEmpty() || colorValue.isEmpty() || price.isEmpty() ||
+                        locationValue.isEmpty() || date.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Ingresar todos los datos", Toast.LENGTH_SHORT).show();
+                } else {
+                    saveRecord(name, lastName, type, detail, order, quantity, colorValue, price, locationValue, date);
+                }
             }
         });
+
+        // Obtener fecha actual
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String currentDate = sdf.format(new Date());
+
+        // Establecer fecha actual en EditText y desactivar edición
+        fecha.setText(currentDate);
+        fecha.setEnabled(false);
+
+        ubicacion.setEnabled(false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView2);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        updateMapLocation();
     }
 
     private void updateMapLocation() {
-        // Check for location permissions
-        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Request the permissions if not granted
-            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE);
         } else {
-            // Get the last known location
             fusedLocationClient.getLastLocation()
                     .addOnSuccessListener(new OnSuccessListener<Location>() {
                         @Override
                         public void onSuccess(Location location) {
                             if (location != null) {
-                                // Save the location in the lastKnownLocation variable
                                 lastKnownLocation = location;
-
-                                // Update the EditText with the location coordinates
                                 ubicacion.setText(location.getLatitude() + ", " + location.getLongitude());
-
-                                // Update the map with a marker at the location
                                 LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                                 googleMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
                                 googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f));
-                            }
-                        }
-                    });
-        }
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        // Primero, asegurarse de tener permisos de ubicación
-        if (ActivityCompat.checkSelfPermission(insertregistro2update.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(insertregistro2update.this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(insertregistro2update.this, new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_CODE);
-        } else {
-            // Si ya tiene los permisos, obtener la ubicación
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null) {
-                                // Guardar la ubicación en la variable lastKnownLocation
-                                lastKnownLocation = location;
-                                ubicacion.setText(location.getLatitude() + " ," + location.getLongitude());
                             }
                         }
                     });
@@ -130,6 +157,33 @@ public class insertregistro2update extends AppCompatActivity {
         onBackPressed();
         return false;
     }
+
+    private void saveRecord(String name, String lastName, String type, String detail, String order, String quantity, String colorValue, String price, String locationValue, String date) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("nombre", name);
+        map.put("apellido", lastName);
+        map.put("tipo", type);
+        map.put("detalle", detail);
+        map.put("pedido", order);
+        map.put("cantidad", quantity);
+        map.put("color", colorValue);
+        map.put("precio", price);
+        map.put("ubicacion", locationValue);
+        map.put("fecha", date);
+
+        mfirestore.collection("mtpdb").add(map)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "Registro Existoso", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error al Ingresar", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
-
-
--- app/src/main/java/com/chakuy/mtp11/minicio2.java
+++ app/src/main/java/com/chakuy/mtp11/minicio2.java
@@ -2,25 +2,22 @@ package com.chakuy.mtp11;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
-import android.net.Uri;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-
-import com.google.firebase.functions.FirebaseFunctions;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.functions.HttpsCallableResult;
-import com.google.android.gms.tasks.OnCompleteListener;
-
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.auth.FirebaseAuth;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -29,181 +26,147 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import android.util.Log;
-
-
-import androidx.annotation.NonNull;
 
 public class minicio2 extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
-    private TextView txtItem;
     private static final String TAG = "minicio2";
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    CollectionReference mtpdbRef = db.collection("mtpdb");
+    private TextView txtItem, txtItem2a;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference mtpdbRef = db.collection("mtpdb");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minicio2);
+
         LinearLayout linearLayoutAgregar = findViewById(R.id.linearLayoutAgregar);
         LinearLayout listado = findViewById(R.id.listado);
         LinearLayout descargar = findViewById(R.id.descargar);
         LinearLayout cerrarsesion = findViewById(R.id.cerrarsesion);
         LinearLayout mapa = findViewById(R.id.mapa);
         txtItem = findViewById(R.id.txtitem);
+        txtItem2a = findViewById(R.id.txtitem2a);
 
-
-        mtpdbRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if (task.isSuccessful()) {
-                    int count = task.getResult().size();
-                    txtItem.setText(String.valueOf(count));
-                    Log.d(TAG, "Cantidad de registros: " + count);
-                } else {
-                    Log.d(TAG, "Error obteniendo documentos: ", task.getException());
-                }
-            }});
-
-        linearLayoutAgregar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(minicio2.this, insertregistro2update.class);
-                startActivity(intent);
-            }
+        linearLayoutAgregar.setOnClickListener(v -> {
+            Intent intent = new Intent(minicio2.this, insertregistro2update.class);
+            startActivity(intent);
         });
 
-        listado.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(minicio2.this, listarpr.class);
-                startActivity(intent);
-            }
+        listado.setOnClickListener(v -> {
+            Intent intent = new Intent(minicio2.this, listarpr.class);
+            startActivity(intent);
         });
 
-        cerrarsesion.setOnClickListener(new View.OnClickListener() {
+        cerrarsesion.setOnClickListener(v -> {
             FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-            @Override
-            public void onClick(View v) {
-                mAuth.signOut();
-                finish();
-                startActivity(new Intent(minicio2.this, login.class));
-            }
+            mAuth.signOut();
+            finish();
+            startActivity(new Intent(minicio2.this, login.class));
         });
 
-        mapa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //startActivity((new Intent(minicio2.this,mapamtp.class)));
-                startActivity((new Intent(minicio2.this,ubicacionmacti.class)));
-            }
+        mapa.setOnClickListener(view -> {
+            startActivity(new Intent(minicio2.this, ubicacionmacti.class));
         });
 
+        descargar.setOnClickListener(view -> {
+            List<MTPData> dataList = new ArrayList<>();
+            Query query = mtpdbRef;
+            query.get().addOnSuccessListener(queryDocumentSnapshots -> {
+                for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                    MTPData data = document.toObject(MTPData.class);
+                    dataList.add(data);
+                }
 
+                Workbook workbook = new XSSFWorkbook();
+                Sheet sheet = workbook.createSheet("Datos MTP");
 
-        descargar.setOnClickListener(new View.OnClickListener() {
+                int rowNum = 0;
+                for (MTPData data : dataList) {
+                    Row row = sheet.createRow(rowNum++);
+                    row.createCell(0).setCellValue(data.getNombre());
+                    row.createCell(1).setCellValue(data.getApellido());
+                    row.createCell(2).setCellValue(data.getFecha());
+                    row.createCell(3).setCellValue(data.getTipo());
+                    row.createCell(4).setCellValue(data.getDetalle());
+                    row.createCell(5).setCellValue(data.getPedido());
+                    row.createCell(6).setCellValue(data.getCantidad());
+                    row.createCell(7).setCellValue(data.getPrecio());
+                    row.createCell(8).setCellValue(data.getUbicacion());
+                    row.createCell(9).setCellValue(data.getColor());
+                }
 
-            @Override
-            public void onClick(View view) {
+                String filePath = getExternalFilesDir(null).getPath().toString() + "/datos_mtp.xlsx";
+                try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
+                    workbook.write(outputStream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
+                Uri fileUri = FileProvider.getUriForFile(minicio2.this, BuildConfig.APPLICATION_ID + ".provider", new File(filePath));
+                grantUriPermission(getPackageName(), fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("application/vnd.ms-excel");
+                intent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(minicio2.this, BuildConfig.APPLICATION_ID + ".provider", new File(filePath)));
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.putExtra(Intent.EXTRA_STREAM, fileUri);
+                startActivity(Intent.createChooser(intent, "Compartir archivo"));
 
-
-                List<MTPData> dataList = new ArrayList<>();
-                FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
-                Query query = mFirestore.collection("mtpdb");
-                query.get().addOnSuccessListener(queryDocumentSnapshots -> {
-                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
-                        MTPData data = document.toObject(MTPData.class);
-                        dataList.add(data);
-                    }
-
-                    // crear un nuevo archivo de Excel
-                    Workbook workbook = new XSSFWorkbook();
-                    Sheet sheet = workbook.createSheet("Datos MTP");
-
-                    // agregar los datos a las celdas
-                    int rowNum = 0;
-                    for (MTPData data : dataList) {
-                        Row row = sheet.createRow(rowNum++);
-                        row.createCell(0).setCellValue(data.getNombre());
-                        row.createCell(1).setCellValue(data.getApellido());
-                        row.createCell(2).setCellValue(data.getFecha());
-                        row.createCell(3).setCellValue(data.getTipo());
-                        row.createCell(4).setCellValue(data.getDetalle());
-                        row.createCell(5).setCellValue(data.getPedido());
-                        row.createCell(6).setCellValue(data.getCantidad());
-                        row.createCell(7).setCellValue(data.getPrecio());
-                        row.createCell(8).setCellValue(data.getUbicacion());
-                        row.createCell(9).setCellValue(data.getColor());
-                    }
-
-                    // guardar el archivo
-                    String filePath = getExternalFilesDir(null).getPath().toString() + "/datos_mtp.xlsx";
-                    try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
-                        workbook.write(outputStream);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    Uri fileUri = FileProvider.getUriForFile(minicio2.this, BuildConfig.APPLICATION_ID + ".provider", new File(filePath));
-                    grantUriPermission(getPackageName(), fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    // abrir el archivo Excel en una aplicación y compartirlo
-                    Intent intent = new Intent(Intent.ACTION_SEND);
-                    intent.setType("application/vnd.ms-excel");
-                    intent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(minicio2.this, BuildConfig.APPLICATION_ID + ".provider", new File(filePath)));
-                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                    intent.putExtra(Intent.EXTRA_STREAM, fileUri);
-                    startActivity(Intent.createChooser(intent, "Compartir archivo"));
-
-                });
-            }
+                updateTotalItemCountByDate();
+            });
         });
 
-        Map<String, Object> data = new HashMap<>();
-        data.put("nombre", "Juan");
-        data.put("apellido", "Pérez");
-        data.put("fecha", "2022-01-01");
-        data.put("tipo", "Producto 1");
-        data.put("detalle", "Descripción del producto 1");
-        data.put("pedido", "Pedido 123");
-        data.put("cantidad", 10);
-        data.put("precio", 100.0);
-        data.put("ubicacion", "Ciudad");
-        data.put("color", "Rojo");
+        updateTotalItemCount();
+        updateTotalItemCountByDate();
+    }
 
+    private void updateTotalItemCount() {
+        mtpdbRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                int count = task.getResult().size();
+                txtItem.setText(String.valueOf(count));
+                Log.d(TAG, "Cantidad de registros: " + count);
+            } else {
+                Log.d(TAG, "Error obteniendo documentos: ", task.getException());
+            }
+        });
+    }
 
-        FirebaseFunctions functions = FirebaseFunctions.getInstance();
+    private void updateTotalItemCountByDate() {
+        String currentDate = getCurrentDateWithoutTime();
+        Query queryByDate = mtpdbRef.whereGreaterThanOrEqualTo("fecha", currentDate).whereLessThan("fecha", getNextDate(currentDate));
+        queryByDate.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            int count = queryDocumentSnapshots.size();
+            txtItem2a.setText(String.valueOf(count));
+            Log.d(TAG, "Cantidad de registros por fecha actual: " + count);
+        }).addOnFailureListener(e -> {
+            Log.d(TAG, "Error obteniendo documentos por fecha actual: " + e.getMessage());
+        });
+    }
 
-        functions.getHttpsCallable("myFunction")
-                .call(data)
-                .addOnCompleteListener(new OnCompleteListener<HttpsCallableResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<HttpsCallableResult> task) {
-                        if (task.isSuccessful()) {
-                            HttpsCallableResult result = task.getResult();
-                            // Procesar el resultado
-                        } else {
-                            Exception e = task.getException();
-                            // Manejar la excepción
-                        }
-                    }
-                });
+    private String getCurrentDateWithoutTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        return sdf.format(new Date());
+    }
 
-
-
-
-
+    private String getNextDate(String date) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+            Date currentDate = sdf.parse(date);
+            long nextDateMillis = currentDate.getTime() + 24 * 60 * 60 * 1000; // Add 24 hours in milliseconds
+            Date nextDate = new Date(nextDateMillis);
+            return sdf.format(nextDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
